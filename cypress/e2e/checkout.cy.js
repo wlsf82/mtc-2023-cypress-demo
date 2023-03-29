eb264d38b41b6d79197dd20f3ed8d6fccec88303
Walmyr <wlsf82@gmail.com>
2023-03-30 01:22:08 +0200

    Adjust checkout scenario to be more "real-life"

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -8,17 +8,41 @@ describe('Checkout', () => {
   it('goes through the whole checkout process successfully', () => {
     // Act
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-    cy.get('.shopping_cart_link').click()
+    cy.contains('.shopping_cart_link span', '1').should('be.visible')
+    cy.contains('button', 'Remove').click()
+    cy.contains('.shopping_cart_link span', '1').should('not.exist')
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.contains('.shopping_cart_link span', '2')
+      .should('be.visible')
+      .click()
     cy.get('[data-test="checkout"]').click()
-    cy.get('[data-test="firstName"]').type('Walmyr')
-    cy.get('[data-test="lastName"]').type('Filho')
-    cy.get('[data-test="postalCode"]').type('12345')
+    cy.get('[data-test="firstName"]')
+      .should('be.empty')
+      .type('Walmyr')
+    cy.get('[data-test="lastName"]')
+      .should('be.empty')
+      .type('Filho')
+    cy.get('[data-test="postalCode"]')
+      .should('be.empty')
+      .type('12345')
     cy.get('[data-test="continue"]').click()
-    cy.get('[data-test="finish"]').click()
+
+    // Assert
+    cy.contains('.summary_subtotal_label', '$39.98').should('be.visible')
     
+    // Act
+    cy.get('[data-test="finish"]').click()
+
     // Assert
     cy.contains('.title', 'Checkout: Complete!').should('be.visible')
     cy.contains('.complete-header', 'Thank you for your order!')
       .should('be.visible')
+
+    // Act
+    cy.get('[data-test="back-to-products"]').click()
+
+    // Assert
+    cy.location('pathname').should('be.equal', '/inventory.html')
   })
 })
