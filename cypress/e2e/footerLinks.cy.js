d6cbb5ba44a2fe88be03068918d8286b0546090f
Walmyr <wlsf82@gmail.com>
2023-03-30 02:00:47 +0200

    Turn three tests into one

--- cypress/e2e/footerLinks.cy.js
+++ cypress/e2e/footerLinks.cy.js
@@ -1,12 +1,12 @@
 import socialItems from "../fixtures/social"
 
 describe('Footer links', () => {
-  socialItems.forEach(({ type, url }) => {
-    it(`checks that ${type} link opens in a new tab`, () => {
-      // Arrange
-      cy.setCookie('session-username', Cypress.env('USERNAME'))
-      cy.visit('/inventory.html', { failOnStatusCode: false })
+  it(`checks that social links opens in a new tab`, () => {
+    // Arrange
+    cy.setCookie('session-username', Cypress.env('USERNAME'))
+    cy.visit('/inventory.html', { failOnStatusCode: false })
 
+    socialItems.forEach(({ type, url }) => {
       // Assert
       cy.get(`.social_${type} a`)
         .should('have.attr', 'href', url)
