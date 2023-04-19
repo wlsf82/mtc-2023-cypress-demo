77590865b68556dabe1c6d12bcf565c3d4244288
Walmyr <wlsf82@gmail.com>
2023-04-19 20:55:32 +0200

    Disable chromeWebSecurity to make tests more stable

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
+    chromeWebSecurity: false,
   },
   experimentalWebKitSupport: true,
 })
