f03d5e7b307e4312dd0e8faeffcd0c73ffb397b0
Walmyr <wlsf82@gmail.com>
2023-12-16 00:10:08 +0100

    Integrate project with the cypress cloud
    And do not save videos on CI since with the Cypress Cloud integra-
    tion we can benefit from the Test Replay functionality.

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: '1zr8eq',
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     chromeWebSecurity: false,
