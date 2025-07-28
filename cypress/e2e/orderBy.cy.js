7b3e69fb90bf09906d6a8a881508fe77c983ef78
Walmyr <wlsf82@gmail.com>
2025-07-28 18:13:14 +0200

    Fix failing test due to app change

--- cypress/e2e/orderBy.cy.js
+++ cypress/e2e/orderBy.cy.js
@@ -6,7 +6,7 @@ describe('Order by', () => {
 
   it('orders by Price (high to low)', () => {
     // Act
-    cy.get('[data-test="product_sort_container"]').select('hilo')
+    cy.get('[data-test="product-sort-container"]').select('hilo')
 
     // Assert
     cy.get('.inventory_item_price')
